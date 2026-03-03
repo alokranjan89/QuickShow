e61d47253cd130cee3c0f6ab98ff2d173295b82d
Alok Ranjan <alokr5393@gmail.com>
2026-03-03 14:50:31 +0530

    Loading Animation Added when Movie id is wrong && movies is taking time to load

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import BlurCircle from '../components/BlurCircle'
 import timeFormat from '../lib/timeFormat'
 import { Heart, PlayCircle, StarIcon } from 'lucide-react'
 import DateSelect from '../components/DateSelect'
+import MovieCard from '../components/MovieCard'
+import Loading from '../components/Loading'
 
 const MovieDetails = () => {
+  const navigate = useNavigate()
   const { id } = useParams()
   const [show, setShow] = useState(null)
 
-  const getShow = () => {
+  const getShow =  () => {
     const foundShow = dummyShowsData.find(show => show._id === id)
 
     if (foundShow) {
@@ -28,6 +31,7 @@ const MovieDetails = () => {
   return show ? (
     <div className='px-6 md:px-16 lg:px-40 pt-30 md:pt-50'>
       
+      {/* Main Section */}
       <div className='flex flex-col md:flex-row gap-8 max-w-6xl mx-auto'>
 
         <img
@@ -88,7 +92,6 @@ const MovieDetails = () => {
       </div>
 
       {/* Cast Section */}
-
       <p className='text-lg font-medium mt-20'>Your favorite cast</p>
 
       <div className='overflow-x-auto no-scrollbar mt-8 pb-4'>
@@ -111,15 +114,29 @@ const MovieDetails = () => {
         </div>
       </div>
 
-      {/* Date Selection Component */}
-
+      {/* Date Selection */}
       <DateSelect dateTime={show.dateTime} id={id} />
+
+      {/* You May Also Like */}
+      <p className="text-lg font-medium mt-20 mb-8">
+        You May Also Like
+      </p>
+
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.slice(0, 4).map((movie) => (
+          <MovieCard key={movie._id} movie={movie} />
+        ))}
+      </div>
+
+      <div className="flex justify-center mt-20">
+        <button onClick={() => {navigate('/movies'); scrollTo(0,0)}} className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer">
+          Show More
+        </button>
+      </div>
 
     </div>
   ) : (
-    <div className="flex justify-center items-center min-h-screen text-gray-400">
-      Loading...
-    </div>
+    <Loading />
   )
 }
 
